186d1d72aa47757e841b0e2c2eae7a03df44f976
tauenow <uruu3246@gmail.com>
2024-10-22 16:14:27 +0900

    PlayerのAnimationController追加

--- Assets/Model/Player/PlayerAnimetion.cs
+++ Assets/Model/Player/PlayerAnimetion.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerAnimetion : MonoBehaviour
 {
     private Animator animator;
+    private float CurrentTime;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,41 @@ public class PlayerAnimetion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CurrentTime += Time.deltaTime;
+        
+       if(CurrentTime >= 10)
+        {
+            Debug.Log("Idleda");
+            Idle();
+            CurrentTime = 0;
+        }
+       else if(CurrentTime >= 5)
+        {
+            Debug.Log("Idleda");
+            Jump(); 
+        }
+
+    }
+   
+    public void Idle()
+    {
+        animator.SetInteger("TransitonNo", 0);
+    }
+
+    public void Yubisasi()
+    {
         
     }
+
+    public void Run()
+    {
+
+    }
+
+    public void Jump()
+    {
+        animator.SetInteger("TransitionNo", 3);
+    }
+
 }
+
